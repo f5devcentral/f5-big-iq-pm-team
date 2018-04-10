95ddf9b7483fc908cf9d38dc28995667bed9bd40
Romain Jouhannet <25542366+rjouhann@users.noreply.github.com>
2018-04-09 20:35:45 -0700

    update pl

--- licenseUtilityReport.pl
+++ licenseUtilityReport.pl
@@ -3,8 +3,6 @@
 #################################################################################
 # Copyright 2018 by F5 Networks, Inc.
 #
-#Copyright 2018 F5 Networks Inc.
-#
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
 #You may obtain a copy of the License at
