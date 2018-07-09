a5ff79ade803fa0215e5c74bb6a273994fe6e758
Romain Jouhannet <r.jouhannet@f5.com>
2018-07-09 08:41:47 -0700

    Updates from Cotney, John

--- f5sanitizeUsageReport/f5_sanitize_usage_report.sh
+++ f5sanitizeUsageReport/f5_sanitize_usage_report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 #################################################################################
 # Copyright 2018 by F5 Networks, Inc.
 #
@@ -15,28 +15,29 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 #################################################################################
-
+ 
 # Uncomment set command below for code debugging bash
 #set -x
-
+ 
 echo -e "\nThe script replace the IP address with “x.x.x.x” and the host name with “redacted-hostname” in a JSON report."
-
+ 
 if [[ -z $1 ]]; then
-
-	echo -e "\n-> No JSON report specified.\n\nUsage: ./f5_sanitize_usage_report.sh report.json\n"
-	exit 1;
-
+ 
+    echo -e "\n-> No JSON report specified.\n\nUsage: ./f5_sanitize_usage_report.sh report.json\n"
+    exit 1;
+ 
 elif [ -f $1 ]; then
-	cp -p $1 $1.orig
-	sed -i '/"address":/c\      "address": "x.x.x.x",' $1
-	sed -i '/"hostname":/c\      "hostname": "redacted-hostname",' $1
-
-	echo -e "\n-> Backup prior modification: $1.orig"
-	echo -e "-> $1 was updated masking address and hostname.\n"
-	exit 0;
+    cp -p $1 $1.orig
+    sed -i -r 's#[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]#x.x.x.x#g' $1
+    sed -i "/'hostname.*,/c\                           \x27hostname\x27: \x27redacted-hostname\x27," $1
+    sed -i "/'hostname.*'/c\                           \x27hostname\x27: \x27redacted-hostname\x27" $1
+ 
+    echo -e "\n-> Backup prior modification: $1.orig"
+    echo -e "-> $1 was updated masking address and hostname.\n"
+    exit 0;
 else
-	
-	echo -e "\n-> JSON report specified “$1” does not exist. Please check filename specified in the argument.\n"
-	ls -l | grep -v total | grep -v $0
-	exit 2;
+    
+    echo -e "\n-> JSON report specified “$1” does not exist. Please check filename specified in the argument.\n"
+    ls -l | grep -v total | grep -v $0
+    exit 2;
 fi
