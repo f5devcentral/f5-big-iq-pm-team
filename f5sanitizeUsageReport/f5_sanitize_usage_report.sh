afdf333854331af79c8c27e79461ad13fd64d321
Romain Jouhannet <rjouhannet@gmail.com>
2018-07-18 07:44:52 -0700

    update hash mac address

--- f5sanitizeUsageReport/f5_sanitize_usage_report.sh
+++ f5sanitizeUsageReport/f5_sanitize_usage_report.sh
@@ -19,7 +19,7 @@
 # Uncomment set command below for code debugging bash
 #set -x
  
-echo -e "\nThe script replace the IP address with “x.x.x.x”,  MAC address with “xx:xx:xx:00:00:00” and the host name with “redacted-hostname” in a JSON report."
+echo -e "\nThe script replaces the IP address with “x.x.x.x”,  MAC address with hash value and the host name with “redacted-hostname” in a JSON report."
  
 if [[ -z $1 ]]; then
  
@@ -27,14 +27,32 @@ if [[ -z $1 ]]; then
     exit 1;
  
 elif [ -f $1 ]; then
-    cp -p $1 $1.orig
-    sed -i -r 's#[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]#x.x.x.x#g' $1
-    sed -i -r 's#[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:#xx:xx:xx:#g' $1
+    # Backup original report
+    mv $1 $1.orig
+    # Setting  internal field separator to null to keep the space
+    IFS='';
+    # Reading file line by line
+    while read -r line; 
+    do 
+        if [[ $line = *"macAddress"* ]]; then
+            # if macAddress, hash it and replace it with the hash
+            macAddressMD5=$(echo $line | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | md5sum | cut -f1 -d' ')
+            echo $line | sed -r "s#[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}#$macAddressMD5#g" >>  $1
+        elif [[ $line = *"address"* ]]; then
+            # if address (IP), replace it with x.x.x.x
+            echo $line | sed -r 's#[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]#x.x.x.x#g' >>  $1
+        else
+            # if no macAddress or address (IP), do nothing
+            echo $line >> $1
+        fi
+    done < $1.orig
+
+    # Replace hostname with redacted-hostnam
     sed -i "/'hostname.*,/c\                           \x27hostname\x27: \x27redacted-hostname\x27," $1
     sed -i "/'hostname.*'/c\                           \x27hostname\x27: \x27redacted-hostname\x27" $1
  
     echo -e "\n-> Backup prior modification: $1.orig"
-    echo -e "-> $1 was updated masking address and hostname.\n"
+    echo -e "-> $1 was updated masking IP addresses, MAC addresses and hostnames.\n"
     exit 0;
 else
     
