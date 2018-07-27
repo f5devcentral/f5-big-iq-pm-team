f48be44f7894e82ad03aa84aa25dd7ea3b403941
Romain Jouhannet <rjouhannet@gmail.com>
2018-07-27 14:39:07 -0700

    v1.2

--- f5sanitizeUsageReport/f5_sanitize_usage_report.sh
+++ f5sanitizeUsageReport/f5_sanitize_usage_report.sh
@@ -18,11 +18,12 @@
 
 # 07/18/2018: v1.0  r.jouhannet@f5.com     Initial version
 # 07/25/2018: v1.1  r.jouhannet@f5.com     Add <full path> in the Usage. also correct redacted-hostname.
+# 07/27/2018: v1.2  r.jouhannet@f5.com     Hash IP address and hostname
 
 # Uncomment set command below for code debugging bash
 #set -x
  
-echo -e "\nThe script replaces the IP address with “x.x.x.x”,  MAC address with hash value and the host name with “redacted-hostname” in a JSON report."
+echo -e "\nThe script replaces the IP address, MAC address and the host name with with hash value (md5) in a JSON report."
  
 if [[ -z $1 ]]; then
  
@@ -40,23 +41,22 @@ elif [ -f $1 ]; then
         if [[ $line = *"macAddress"* ]]; then
             # if macAddress, hash it and replace it with the hash
             macAddressMD5=$(echo $line | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | md5sum | cut -f1 -d' ')
-            echo $line | sed -r "s#[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}#$macAddressMD5#g" >>  $1
+            echo $line | sed -r "s#[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}#$macAddressMD5#g" >> $1
         elif [[ $line = *"address"* ]]; then
-            # if address (IP), replace it with x.x.x.x
-            echo $line | sed -r 's#[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]#x.x.x.x#g' >>  $1
+            # if address (IP), hash it and replace it with the hash
+            ipAddressMD5=$(echo $line | grep -o -E '[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]' | md5sum | cut -f1 -d' ')
+            echo $line | sed -r "s#[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]#$ipAddressMD5#g" >> $1
+        elif [[ $line = *"hostname"* ]]; then
+             # hostname, hash it and replace it with the hashx22=" x27='
+            hostname=$(echo $line | awk -F'"' '$2=="hostname"{print $4}')
+            hostnameMD5=$(echo $line | awk -F'"' '$2=="hostname"{print $4}' | md5sum | cut -f1 -d' ')
+            echo $line | sed -r "s#$hostname#$hostnameMD5#g" >> $1
         else
             # if no macAddress or address (IP), do nothing
             echo $line >> $1
         fi
     done < $1.orig
 
-    # Replace hostname with redacted-hostname x22=" x27='
-    sed -i "s#.*\x22hostname.*,#      \x22hostname\x22: \x22redacted-hostname\x22,#" $1
-    sed -i "s#.*\x22hostname.*\x22#      \x22hostname\x22: \x22redacted-hostname\x22#" $1
-
-    sed -i "s#.*\x27hostname.*,#      \x27hostname\x27: \x27redacted-hostname\x27,#" $1
-    sed -i "s#.*\x27hostname.*\x27#      \x27hostname\x27: \x27redacted-hostname\x27#" $1
- 
     echo -e "\n-> Backup prior modification: $1.orig"
     echo -e "-> $1 was updated masking IP addresses, MAC addresses and hostnames.\n"
     exit 0;
