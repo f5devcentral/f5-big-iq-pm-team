a8b2c8e768d858e5ae830fbdcdf8f0737fbfb272
Roman Jouhannet <r.jouhannet@f5.com>
2020-04-07 18:32:04 -0700

    update Copyright

--- f5-bigiq-licenseUtilityReport/licenseUtilityReport.pl
+++ f5-bigiq-licenseUtilityReport/licenseUtilityReport.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/perl -w
 
 #################################################################################
-# Copyright 2018 by F5 Networks, Inc.
+# Copyright 2020 by F5 Networks, Inc.
 #
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
--- f5-bigiq-sanitizeUsageReport/f5_sanitize_usage_report.sh
+++ f5-bigiq-sanitizeUsageReport/f5_sanitize_usage_report.sh
@@ -3,7 +3,7 @@
 #set -x
 
 #################################################################################
-# Copyright 2018 by F5 Networks, Inc.
+# Copyright 2020 by F5 Networks, Inc.
 #
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
