bff9fbf1bbcf827a348e78904b6e1b8061e1f83d
Roman Jouhannet <r.jouhannet@f5.com>
2020-09-15 17:13:23 -0700

    Remove adding rpmFilePath in restjavad.properties.json

--- f5-bigiq-as3-rpm-update/as3_rpm_update_bigiq.sh
+++ f5-bigiq-as3-rpm-update/as3_rpm_update_bigiq.sh
@@ -20,14 +20,16 @@
 
 # 04/07/2020: v1.0  r.jouhannet@f5.com    Initial version
 # 04/08/2020: v1.1  r.jouhannet@f5.com    Add $home/ in the if rpm -Uv --force 
+# 09/15/2020: v1.2  r.jouhannet@f5.com    Remove adding rpmFilePath in restjavad.properties.json 
+#                                         BIG-IQ will use the newest AS3 RPM available in /usr/lib/dco/packages/f5-appsvcs/
 
 home="/home/admin"
 
 if [[ -z $1 ]]; then
     echo -e "\nRead https://support.f5.com/csp/article/K54909607\n"
-    echo -e "Usage: $0 <newRPM> <currentRPM>\n"
+    echo -e "Usage: $0 <newRPM>\n"
     echo -e "Note: new AS3 RPM needs to be uploaded in /home/admin.\n"
-    echo -e "Example: $0 f5-appsvcs-3.18.0-4.noarch.rpm f5-appsvcs-3.13.2-1.noarch.rpm\n"
+    echo -e "Example: $0 f5-appsvcs-3.18.0-4.noarch.rpm\n"
     echo "AS3 version currently installed:"
     curl -s http://localhost:8105/shared/appsvcs/info | jq .
     echo
@@ -37,15 +39,16 @@ if [[ -z $1 ]]; then
     echo "AS3 RPM in /usr/lib/dco/packages/f5-appsvcs:"
     ls -l /usr/lib/dco/packages/f5-appsvcs/*rpm
     echo
-    echo "AS3 rpmFilePath configured in /var/config/rest/config/restjavad.properties.json:"
-    cat /var/config/rest/config/restjavad.properties.json | jq .global.appSvcs
-    echo
+    c=$(grep rpmFilePath /var/config/rest/config/restjavad.properties.json | wc -l)
+    if [[ $c == 1 ]]; then
+        echo "AS3 rpmFilePath configured in /var/config/rest/config/restjavad.properties.json:"
+        cat /var/config/rest/config/restjavad.properties.json | jq .global.appSvcs
+        echo
+    fi
     exit 1
 fi
 
 newRPM="$1"
-currentRPM="$2" #optional for initial install
-
 
 if [ -f $home/$newRPM ]; then
     echo -e "\nInstalling latest RPM: ${newRPM}"
@@ -55,23 +58,32 @@ if [ -f $home/$newRPM ]; then
     if rpm -Uv --force "$home/$newRPM" ; then
         mount -o remount,rw /usr
         echo "Updating restjavad props to point to new RPM"
-        if [ -z "$currentRPM" ]; then
-            currentRPM=$newRPM
-        fi
-        c=$(grep $currentRPM /var/config/rest/config/restjavad.properties.json | wc -l)
+
+        c=$(grep rpmFilePath /var/config/rest/config/restjavad.properties.json | wc -l)
         if [[ $c == 1 ]]; then
-            sed -i "s/$currentRPM/$newRPM/g" /var/config/rest/config/restjavad.properties.json
-            rm -rf /usr/lib/dco/packages/f5-appsvcs/$currentRPM
+            echo "Remove any rpmFilePath reference in restjavad.properties.json"
+            # backup original file
+            cp -rp /var/config/rest/config/restjavad.properties.json /var/config/rest/config/restjavad.properties.json.$(date +%Y-%m-%d_%H-%M)
+            # Nice JSON
+            cat /var/config/rest/config/restjavad.properties.json | jq . > /var/config/rest/config/restjavad.properties.json.tmp
+            cp /var/config/rest/config/restjavad.properties.json.tmp /var/config/rest/config/restjavad.properties.json
+            rm -f /var/config/rest/config/restjavad.properties.json.tmp
+            # remove rpmFilePath if it's there
+            sed -i '/rpmFilePath/d' /var/config/rest/config/restjavad.properties.json
+            # check
+            cat /var/config/rest/config/restjavad.properties.json | jq .global.appSvcs
+            #rm -rf /usr/lib/dco/packages/f5-appsvcs/$currentRPM
             mv $home/$newRPM /usr/lib/dco/packages/f5-appsvcs/
         else
-            sed -i "s#\"appMappingPollingTimeOutSeconds\" : 180#\"appMappingPollingTimeOutSeconds\" : 180,\"rpmFilePath\" : \"/usr/lib/dco/packages/f5-appsvcs/$newRPM\"#g" /var/config/rest/config/restjavad.properties.json
             mv $home/$newRPM /usr/lib/dco/packages/f5-appsvcs/
         fi
         mount -o remount,ro /usr
+        echo "Restart restjavad..."
         bigstart restart restjavad &
         restartProc=$!
         wait $restartProc
         sleep 5
+        echo "Restart restnoded..."
         bigstart restart restnoded &
         restartProc=$!
         wait $restartProc
